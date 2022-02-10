19cc5de65f19b1350acc3e9e14decc7782a04509
Shivraj <shivunofficial003@gmail.com>
2022-02-10 20:34:36 +0530

    making folders

---
+++ Command Function/command.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("\nListening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognising...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        # speak("Say that again please")
+        return "None"
+
+    return query
