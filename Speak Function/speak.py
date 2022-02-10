977b302bcdfa556c916470d5a81e4e7872c42dea
Shivraj <shivunofficial003@gmail.com>
2022-02-10 20:24:25 +0530

    updates

---
+++ Speak Function/speak.py
@@ -0,0 +1,13 @@
+import pyttsx3 
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 170)
+# print(voices)
+
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
